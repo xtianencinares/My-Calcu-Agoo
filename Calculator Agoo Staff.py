15ec2b24b52476733f2fd45c4dafd0cbfeabd9a6
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2024-06-03 09:56:03 +0800

    Update Calculator Agoo Staff.py

--- Calculator Agoo Staff.py
+++ Calculator Agoo Staff.py
@@ -46,8 +46,7 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
         
-        # check if user wants another calculation
-        # break the while loop if answer is no
+
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
